c7df425227d99330ce27d7b25de3faa1de782aa6
Lee Li <lee0210@outlook.com>
2016-11-22 20:21:07 +0800

    complete first version

--- main.py
+++ main.py
@@ -1,21 +1,57 @@
 import socket
 import os
+import plock
 from plock_conf import socket_path
-from plock import parse
 
+EXIT = 'E'
 os.unlink(socket_path)
 
 server_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 server_socket.bind(socket_path)
 server_socket.listen(10)
 
+bulk_lock = []
+waiting_queue = {}
+
+def send_message_and_close(s, d):
+    try: 
+        s.send(d) 
+        return True
+    except:
+        return False
+    finally:
+        s.close()
+
 terminal = False
+
 while not terminal:
     client_socket, addr = server_socket.accept()
-    request = client_socket.recv(1024)
-    result = parse(request)
-    print result
-    client_socket.send('OK')
-    client_socket.close()
-
-
+    option, data = plock.parse_request(client_socket)
+    if option == plock.LOCK:
+        if data in bulk_lock:
+            if not waiting_queue.has_key(data): waiting_queue[data] = []
+            waiting_queue[data].append(client_socket)
+        else:
+            bulk_lock.append(data)
+            send_message_and_close(client_socket, plock.SUCCESS)
+    if option == plock.UNLOCK:
+        if data in bulk_lock:
+            no_pending = True
+            if waiting_queue.has_key(data):
+                wq = waiting_queue[data]
+                try:
+                    cs = wq.pop(0)
+                    while not send_message_and_close(cs, plock.SUCCESS):
+                        cs = wq.pop(0)   
+                    no_pending = False
+                except IndexError, e:
+                    waiting_queue.pop(data)
+            if no_pending:
+                bulk_lock.remove(data)
+            send_message_and_close(client_socket, plock.SUCCESS)
+        else:
+            send_message_and_close(client_socket, plock.NOLOCK)
+    if option == EXIT:
+        terminal = True
+        send_message_and_close(client_socket, plock.SUCCESS)
+    
--- plock.py
+++ plock.py
@@ -5,52 +5,61 @@ from exceptions import RuntimeError
 from plock_conf import socket_path
 import socket
 
-all_format = '>c32si{0}s'
-header_format = '>c32si'
+all_format = '>ci{0}s'
+header_format = '>ci'
 data_format = '>{0}s'
 LOCK = 'L'
 UNLOCK = 'U'
+EXIT = 'E'
+SUCCESS = 'OK'
+NOLOCK = 'NOLOCK'
 
 def _send_request(request, time_out=None):
     client_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     client_socket.connect(socket_path)
     client_socket.send(request)
     client_socket.settimeout(time_out)
-    result = client_socket.recv(1024)
-    if result == '': result = False
-    client_socket.close()
+    try:
+        result = client_socket.recv(1024)
+    except Exception, e:
+        result = 'FAILED'
+    finally:
+        client_socket.close()
     return result
 
 def lock(*args, **dargs):
-    time_out = None
-    if dargs.has_key('time_out'): time_out = dargs['time_out']
+    if args is (): raise RuntimeError('key is required')
+    time_out = dargs['time_out'] if dargs.has_key('time_out') else None
     key = str(args)
-    request =  pack(all_format.format(len(key)), LOCK, ' '*32, len(key), key)
-    _send_request(request, time_out)
+    request =  pack(all_format.format(len(key)), LOCK, len(key), key)
+    return _send_request(request, time_out)
 
-def bulk_lock(bulk_id=None, *args):
-    if bulk_id is None and args is not ():
-        raise RuntimeError('bulk_id required')
-    if bulk_id is None:
-        return uuid1()
+def bulk_lock(bulk_id, *args, **dargs):
+    if args is not (): raise RuntimeError('key is required')
+    bulk_id.append(args)
+    return lock(*args, **dargs)
+
+def unlock(*args, **dargs):
+    if args is (): raise RuntimeError('key is required')
+    time_out = dargs['time_out'] if dargs.has_key('time_out') else None
     key = str(args)
-    request = pack(all_format.format(len(key)), LOCK, bulk_id.get_hex(), len(key), key)
+    request = pack(all_format.format(len(key)), UNLOCK, len(key), key)
+    return _send_request(request, time_out)
 
-def unlock(*args):
-    key = str(args)
-    request = pack(all_format.format(len(key)), UNLOCK, ' '*32, len(key), key)
+def bulk_unlock(bulk_id, time_out):
+    result = True
+    for l in bulk_id:
+        if unlock(*l, time_out=time_out): result = False
+    bulk_id = []
+    return result
 
-def bulk_lock(bulk_id=None, *args):
-    if bulk_id is None and args is not ():
-        raise RuntimeError('bulk_id required')
-    if bulk_id is None:
-        return uuid1()
-    key = str(args)
-    request = pack(all_format.format(len(key)), UNLOCK, bulk_id.get_hex(), len(key), key)
+def parse_request(client_socket):
+    option, data_length = unpack(header_format, client_socket.recv(5))
+    data = ''
+    if data_length > 0:
+        data, = unpack(data_format.format(data_length), client_socket.recv(data_length))
+    return option, data
 
-
-def parse(request):
-    option, bulk_id, data_length = unpack(header_format, request[0:37])
-    data, = unpack(data_format.format(data_length), request[37:])
-    return {'option': option, 'bulk_id': bulk_id, 'data': data}
+def exit_program():
+    return _send_request(pack(header_format, EXIT, 0))
 
